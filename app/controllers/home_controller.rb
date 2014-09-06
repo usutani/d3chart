ee4a1f3847de479eb563f2e3ed6ed4e139fa8e47
Yasuhiro Usutani <usutani@tgl.co.jp>
2014-09-06 15:37:50 +0900

    Bar chart

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,30 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def list
+    data = [
+      {
+        'category' => 'A',
+        'amount' => '90'
+      },
+      {
+        'category' => 'B',
+        'amount' => '40'
+      },
+      {
+        'category' => 'C',
+        'amount' => '15'
+      },
+      {
+        'category' => 'D',
+        'amount' => '10'
+      },
+      {
+        'category' => 'E',
+        'amount' => '7'
+      }
+    ]
+    render json: data
+  end
+end
